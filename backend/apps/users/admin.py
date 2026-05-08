7eab7921ad5497d34b26fd5b30212ff2a1cb7506
NGHLakshan <nghlakshan22@gmail.com.noreply.github.com>
2026-05-08 13:55:06 +0530

    update 6

--- backend/apps/users/admin.py
+++ backend/apps/users/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+    list_display = UserAdmin.list_display + ('role',)
+
+admin.site.register(User, CustomUserAdmin)
